c4f9c914ff63a077a18b138419f0f46ac218a106
tvanbesi <tvanbesi@student.42.fr>
2021-12-08 21:35:27 +0100

    adds ft::vector public member functions rbegin() and rend()

--- vector.hpp
+++ vector.hpp
@@ -123,6 +123,10 @@ namespace ft {
 		const_iterator begin() const { return const_iterator(_vector); }
 		iterator end() { return iterator(_vector + _size); }
 		const_iterator end() const { return const_iterator(_vector + _size); }
+		reverse_iterator rbegin() { return reverse_iterator(this->end()); }
+		const_reverse_iterator rbegin() const { return const_reverse_iterator(this->end()); }
+		reverse_iterator rend() { return reverse_iterator(this->begin()); }
+		const_reverse_iterator rend() const { return const_reverse_iterator(this->begin()); }
 
 		/*
 		**	Capacity
