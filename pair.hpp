b74e4489f0f085dfd49d66bdbd0324bd0fe490f3
tvanbesi <tvanbesi@student.42.fr>
2021-12-10 19:46:51 +0100

    adds ft::make_pair function template

--- pair.hpp
+++ pair.hpp
@@ -44,6 +44,13 @@ namespace ft {
 
 	};
 
+	/*
+	**	make_pair function template
+	*/
+
+	template <class T1, class T2>
+	pair<T1, T2> make_pair(T1 x, T2 y) { return (pair<T1, T2>(x, y)); }
+
 }
 
 #endif
