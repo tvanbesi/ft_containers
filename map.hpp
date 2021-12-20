15635b036693e4a9a9edb3989875d790d835ffd7
tvanbesi <tvanbesi@student.42.fr>
2021-12-20 18:49:51 +0100

    adds ft::map::value_comp() getter

--- map.hpp
+++ map.hpp
@@ -38,6 +38,21 @@ namespace ft {
 		typedef				pair<const key_type, mapped_type>				value_type;
 		typedef				Compare											key_compare;
 		typedef				Alloc											allocator_type;
+		class value_compare : std::binary_function<value_type, value_type, bool>
+		{
+			friend class map;
+		protected:
+			Compare comp;
+			value_compare (Compare c) : comp(c) {}
+		public:
+			typedef	bool		result_type;
+			typedef	value_type	first_argument_type;
+			typedef	value_type	second_argument_type;
+			bool operator() (const value_type& x, const value_type& y) const
+			{
+				return comp(x.first, y.first);
+			}
+		};
 		typedef typename	allocator_type::reference						reference;
 		typedef typename	allocator_type::const_reference					const_reference;
 		typedef typename	allocator_type::pointer							pointer;
